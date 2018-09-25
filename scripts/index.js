55b49436916facdd6168fd70eb66b14f77a0c714
Jeffrey James Howarth <jeffhowarth31@gmail.com>
2018-09-24 23:30:51 -0700

    S5 Menu

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@
 
 const UI = (function () {
 
+    let menu = document.querySelector("#menu-container");
+
     const showApp = () => {
         document.querySelector("#app-loader")
         .classList.add('display-none');
@@ -16,6 +18,14 @@ const UI = (function () {
         document.querySelector("main").setAttribute
         ('hidden', 'true');
     };
+
+    const _showMenu = () => menu.style.right = 0;
+
+    const _hideMenu = () => menu.style.right = '-65%';
+
+    document.querySelector("#open-menu-btn").addEventListener('click', _showMenu);
+
+    document.querySelector("#close-menu-btn").addEventListener('click', _hideMenu);
 
     return{
         showApp,
